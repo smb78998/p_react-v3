19acf6175d295c65128df652ab5139a4d97d8ed1
smb7898 <smb78998@gmail.com>
2024-02-20 15:38:07 -0500

    first commit

---
+++ react-v3/src/App.jsx
@@ -0,0 +1,9 @@
+import Contact from './Contact.jsx'
+
+function App() {
+  return(
+    <Contact></Contact>
+  );
+}
+
+export default App
---
+++ react-v3/src/Contact.jsx
@@ -0,0 +1,19 @@
+
+function Contact(){
+
+  return(
+   
+    <section className="contact">
+       <h2 className='heading-2'>Contact us </h2>
+      <ul className="p">
+        <li >123-456-7899</li>
+        <li>cashchronicles@money.com</li>
+        <li>Real Street 123, Orlando FL</li>
+      </ul>
+    </section>
+
+  );
+
+}
+
+export default Contact 
